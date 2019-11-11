ffb8990eeea8aaf1ad6c77145a46f9916aaa088a
billaud <billaudwilliam@gmail.com>
2019-11-11 18:01:38 +0100

    correction son hdmi

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install i3
+sudo apt-get install  i3
+
+# instqll urxvt
+
+# install zsh
+
+# install needed font for agnoster
+# adobe  source code vqriable https://github.com/adobe-fonts/source-code-pro
